92c2ab9ad29ca9ea37736f2cde6410b95ec94125
Juraj Andrassy <juraj.andrassy@gmail.com>
2023-10-15 09:21:12 +0200

    WiFiClient.h - all TCP state constants

--- src/WiFiClient.h
+++ src/WiFiClient.h
@@ -27,7 +27,15 @@
 enum WiFiTcpState {
   CLOSED      = 0,
   LISTEN      = 1,
-  ESTABLISHED = 4
+  SYN_SENT    = 2,
+  SYN_RCVD    = 3,
+  ESTABLISHED = 4,
+  FIN_WAIT_1  = 5,
+  FIN_WAIT_2  = 6,
+  CLOSE_WAIT  = 7,
+  CLOSING     = 8,
+  LAST_ACK    = 9,
+  TIME_WAIT   = 10
 };
 
 class WiFiServer;
