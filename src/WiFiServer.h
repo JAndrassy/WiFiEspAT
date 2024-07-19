4123969666e0cb3a3f06061dfee5634cd4f99c62
Juraj Andrassy <juraj.andrassy@gmail.com>
2024-07-19 17:32:41 +0200

    WiFiServer - defines for max clients and clients timeout

--- src/WiFiServer.h
+++ src/WiFiServer.h
@@ -22,17 +22,25 @@
 
 #include "WiFiClient.h"
 
+#ifndef WIFIESPAT_SERVER_MAX_CLIENTS
+#define WIFIESPAT_SERVER_MAX_CLIENTS 1
+#endif
+
+#ifndef WIFIESPAT_SERVER_CLIENT_TIMEOUT
+#define WIFIESPAT_SERVER_CLIENT_TIMEOUT 60  // seconds
+#endif
+
 class WiFiServer {
 
 public:
   WiFiServer(uint16_t port = 80);
-  void begin() { begin(1, 60); }
+  void begin() { begin(WIFIESPAT_SERVER_MAX_CLIENTS, WIFIESPAT_SERVER_CLIENT_TIMEOUT); }
   void begin(uint8_t maxConnCount, uint16_t serverTimeout);
-  void begin(uint16_t port) { begin(port, 1, 60); }
+  void begin(uint16_t port) { begin(port, WIFIESPAT_SERVER_MAX_CLIENTS, WIFIESPAT_SERVER_CLIENT_TIMEOUT); }
   void begin(uint16_t port, uint8_t maxConnCount, uint16_t serverTimeout);
-  void beginSSL() { beginSSL(false, 1, 60); }
+  void beginSSL() { beginSSL(false, WIFIESPAT_SERVER_MAX_CLIENTS, WIFIESPAT_SERVER_CLIENT_TIMEOUT); }
   void beginSSL(bool ca, uint8_t maxConnCount, uint16_t serverTimeout);
-  void beginSSL(uint16_t port) { beginSSL(port, false, 1, 60); }
+  void beginSSL(uint16_t port) { beginSSL(port, false, WIFIESPAT_SERVER_MAX_CLIENTS, WIFIESPAT_SERVER_CLIENT_TIMEOUT); }
   void beginSSL(uint16_t port, bool ca, uint8_t maxConnCount, uint16_t serverTimeout);
   void end();
   uint8_t status();
